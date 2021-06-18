30701909c0ed4bd114b589f865397ec01a17fa6a
Zaplatunskuj <v.zaplatunskuj@crosssoft.de>
2021-06-18 13:44:43 +0300

    3: generate basic components

---
+++ src/app/components/main/asked-question/asked-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asked-question',
+  templateUrl: './asked-question.component.html',
+  styleUrls: ['./asked-question.component.scss']
+})
+export class AskedQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/main/your-answer/your-answer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-your-answer',
+  templateUrl: './your-answer.component.html',
+  styleUrls: ['./your-answer.component.scss']
+})
+export class YourAnswerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
